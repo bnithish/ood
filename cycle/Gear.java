b59724d402111bf3725c8c59dfddf6c3ef61d54e
bnithishr <nithish25061999@gmail.com>
2020-07-15 16:05:27 +0530

    made the methods have single responsibity and encapsulated local varibles to enable easy change

--- cycle/Gear.java
+++ cycle/Gear.java
@@ -1,6 +1,19 @@
 class Gear{
 	private int chainring,cog,rim;
 	private double tire;
+	public int chainring(){
+		return this.chainring;
+	}
+	public int cog(){
+                return this.cog;
+        }
+	public int rim(){
+                return this.rim;
+        }
+	public double tire(){
+                return this.tire;
+        }
+
 	public Gear(int chainring,int cog,int rim,double tire){
 		this.chainring = chainring;
 		this.cog = cog;
@@ -8,10 +21,13 @@ class Gear{
 		this.tire = tire;
 	}
 	public double ratio(){
-		return this.chainring/(double)this.cog;
+		return this.chainring()/(double)this.cog();
 	}
 	public double gear_inches(){
-		return this.ratio() * (this.rim + (this.tire * 2));
+		return this.ratio() * this.diameter();
+	}
+	public double diameter(){
+		return this.rim() + (2 * this.tire());
 	}
 }
 

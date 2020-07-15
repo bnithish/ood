0b3a8468abab1026e4d44dfd8af2e792f5f48efd
bnithishr <nithish25061999@gmail.com>
2020-07-15 15:46:24 +0530

    added gear_inches functionality to gear class

--- cycle/Gear.java
+++ cycle/Gear.java
@@ -1,11 +1,17 @@
 class Gear{
-	private int chainring,cog,wheel;
-	public Gear(int chainring,int cog){
+	private int chainring,cog,rim;
+	private double tire;
+	public Gear(int chainring,int cog,int rim,double tire){
 		this.chainring = chainring;
 		this.cog = cog;
+		this.rim = rim;
+		this.tire = tire;
 	}
-	public float ratio(){
-		return this.chainring/(float)this.cog;
+	public double ratio(){
+		return this.chainring/(double)this.cog;
+	}
+	public double gear_inches(){
+		return this.ratio() * (this.rim + (this.tire * 2));
 	}
 }
 
--- cycle/test.java
+++ cycle/test.java
@@ -1,8 +1,8 @@
 class test{
 	public static void main(String[] args){
-		Gear gear = new Gear(52,11);
-		System.out.println(gear.ratio());
-		Gear gear2 = new Gear(30,27);
-		System.out.println(gear2.ratio());
+		Gear gear = new Gear(52,11,26,1.5);
+		System.out.println(gear.gear_inches());
+		Gear gear2 = new Gear(52,11,24,1.25);
+		System.out.println(gear2.gear_inches());
 	}
 }
